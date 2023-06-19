8014e21392e324476f74690116295496c4884aed
chetan sharma <108821545+C-H-E-T-A-N@users.noreply.github.com>
2023-06-19 16:10:15 +0530

    final commit

--- script.js
+++ script.js
@@ -1,91 +1,9 @@
 function handleDonation() {
-    var amountInput = document.getElementById('amount');
-    var amount = amountInput.value;
-    var emailInput = document.getElementById('email');
-  var email = emailInput.value;
- 
+    // ... your code for handling the donation ...
   
-    if (amount <= 0) {
-      alert('Please enter a valid amount.');
-      return;
-    }
-  
-    var options = {
-      key: 'rzp_test_scu41OLf6iIFF9',
-      amount: amount * 100,  // Amount in paise
-      currency: 'INR',
-      name: 'Your Organization',
-      description: 'Donation for a cause',
-      image: 'your_logo.png',
-      handler: function(response) {
-          // Handle the payment success
-          var paymentId = response.razorpay_payment_id;
-          var orderId = response.razorpay_order_id;
-          var signature = response.razorpay_signature;
-          
-          // Send an invoice to the user's email address
-          sendInvoice(paymentId, orderId, signature);
-        },
-        email: email,
-      prefill: {
-        email: email,
-    
-      },
-      notes: {
-        donation_type: 'General Donation'
-      }
-    };
-  
-    var razorpayPayment = new Razorpay(options);
-    razorpayPayment.open();
+    // Redirect the user to the desired link
+    window.location.href = 'https://pages.razorpay.com/pl_M33VR0moxuyunC/view'; // Replace with your desired link
   }
-  
-  function sendInvoice(paymentId, orderId, signature) {
-    var emailInput = document.getElementById('email');
-    var email = emailInput.value;
-  
-    var invoiceData = {
-      payment_id: paymentId,
-      order_id: orderId,
-      description: 'Invoice for your donation',
-      customer: {
-        email: email
-      },
-      line_items: [
-        {
-          name: 'Donation',
-          description: 'Donation for a cause',
-          amount: amount * 100, // Amount in paise
-          currency: 'INR',
-          quantity: 1
-        }
-      ]
-    };
-  
-    fetch('https://api.razorpay.com/v1/invoices', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Basic ' + btoa('rzp_test_scu41OLf6iIFF9:')
-      },
-      body: JSON.stringify(invoiceData)
-    })
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(data) {
-      var invoiceUrl = data.invoice.short_url;
-      console.log('Invoice URL:', invoiceUrl);
-      // You can implement your logic to send the invoice URL to the user's email here
-      // Example using window.open to open the invoice URL in a new tab
-      window.open(invoiceUrl, '_blank');
-    })
-    .catch(function(error) {
-      console.error('Error creating invoice:', error);
-    });
-  }
-  
-
   document.getElementById('navbar-toggler').addEventListener('click', function() {
     var navbarMenu = document.getElementById('navbar-menu');
     var navbarToggler = document.getElementById('navbar-toggler');
--- script2.js
+++
@@ -1,7 +0,0 @@
-function handleDonation() {
-    // ... your code for handling the donation ...
-  
-    // Redirect the user to the desired link
-    window.location.href = 'https://pages.razorpay.com/pl_M33VR0moxuyunC/view'; // Replace with your desired link
-  }
-  
